b62b235be4f5e95b6a27b20745825e06916f0d4d
Alexandre Calmon d3vB4 <alexandre.s.junior6@ba.estudante.senai.br>
2025-09-17 02:15:21 -0300

    ajuste admin e exe

--- run.py
+++ run.py
@@ -1,9 +1,23 @@
 # run.py
 from app import create_app
+from waitress import serve
+import webbrowser
 
+# Cria a instância da aplicação
 app = create_app()
 
 if __name__ == '__main__':
-    # O modo debug não é recomendado para produção.
-    app.run(debug=True)
-
+    # Define o endereço e a porta
+    host = '127.0.0.1'
+    port = 8000
+    
+    # Monta a URL completa
+    url = f"http://{host}:{port}"
+    
+    # Abre o navegador na URL da aplicação um pouco antes de iniciar o servidor
+    webbrowser.open(url)
+    
+    print(f"Servidor iniciado. Acesse a aplicação em: {url}")
+    
+    # Inicia o servidor de produção Waitress
+    serve(app, host=host, port=port)
